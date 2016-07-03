bdb94c0e93eda2982e735f12bff7992889e2578c
douglasskiser <douglass.kiser@me.com>
2016-07-03 11:50:20 -0500

    update module exports

--- dist/stats.js
+++ dist/stats.js
@@ -4,9 +4,51 @@ var _harmonicMean = require('./modules/harmonic-mean');
 
 var _harmonicMean2 = _interopRequireDefault(_harmonicMean);
 
+var _geometricMean = require('./modules/geometric-mean');
+
+var _geometricMean2 = _interopRequireDefault(_geometricMean);
+
+var _median = require('./modules/median');
+
+var _median2 = _interopRequireDefault(_median);
+
+var _midRange = require('./modules/mid-range');
+
+var _midRange2 = _interopRequireDefault(_midRange);
+
+var _range = require('./modules/range');
+
+var _range2 = _interopRequireDefault(_range);
+
+var _populationVariance = require('./modules/population-variance');
+
+var _populationVariance2 = _interopRequireDefault(_populationVariance);
+
+var _sampleStandardDeviation = require('./modules/sample-standard-deviation');
+
+var _sampleStandardDeviation2 = _interopRequireDefault(_sampleStandardDeviation);
+
+var _sampleVariance = require('./modules/sample-variance');
+
+var _sampleVariance2 = _interopRequireDefault(_sampleVariance);
+
+var _standardError = require('./modules/standard-error');
+
+var _standardError2 = _interopRequireDefault(_standardError);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-module.export = {
-  harmonicMean: _harmonicMean2.default
+var stats = {
+  harmonicMean: _harmonicMean2.default,
+  geometricMean: _geometricMean2.default,
+  median: _median2.default,
+  midRange: _midRange2.default,
+  range: _range2.default,
+  populationVariance: _populationVariance2.default,
+  sampleStandardDeviation: _sampleStandardDeviation2.default,
+  sampleVariance: _sampleVariance2.default,
+  standardError: _standardError2.default
 };
+
+module.export = stats;
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import harmonicMean from './modules/harmonic-mean';
+import geometricMean from './modules/geometric-mean';
+import median from './modules/median';
+import midRange from './modules/mid-range';
+import range from './modules/range';
+import populationVariance from './modules/population-variance';
+import sampleStandardDeviation from './modules/sample-standard-deviation';
+import sampleVariance from './modules/sample-variance';
+import standardError from './modules/standard-error';
 
-module.export = {
-  harmonicMean
+const stats = {
+  harmonicMean,
+  geometricMean,
+  median,
+  midRange,
+  range,
+  populationVariance,
+  sampleStandardDeviation,
+  sampleVariance,
+  standardError
 };
+
+module.export = stats;
